000e9992df1bdfc89a9b4f2eb4ce4a953f84287b
karan dehghani <karan@karans-mbp-2.speedport_w_921v_1_46_000>
2020-02-17 22:47:14 +0100

    added PG essay Noob

---
+++ src/containers/Noob/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Nav from '../../components/Nav/nav.js';
+
+    //const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    class Email extends React.Component {
+      render() {
+        return (
+        <div className="App">
+          <header className="App-header">
+          </header>
+          <body>
+            <div id="menu">
+              <Nav />
+            </div>
+            <div id="content">
+            <p><a href="http://paulgraham.com/noob.html">Paul Graham's Aufsatz: Being a Noob auf Deutsch</a></p>
+
+<p>Januar 2020</p>
+<p>Als ich jung war, dachte ich, dass die Älteren alles im Griff hätten. Jetzt, wo ich alt bin, weiß ich, dass das nicht stimmt.
+</p>
+<p>Ich fühle mich ständig wie ein Neuling. Es scheint, als ob ich immer mit einem Start-up spreche, das in einem neuen Bereich arbeitet, über den ich nichts weiß, oder ein Buch über ein Thema lese, das ich nicht gut genug verstehe, oder ein neues Land besuche, in dem ich nicht weiß, wie die Dinge funktionieren.
+</p>
+<p>Es ist nicht angenehm, sich wie ein Neuling zu fühlen. Und das Wort "Neuling" ist sicherlich kein Kompliment. Und doch ist mir heute etwas Ermutigendes am Neuling-Sein klar geworden: Je mehr man lokal ein Neuling ist, desto weniger ist man global ein Neuling.
+</p>
+<p>Wenn Sie zum Beispiel in Ihrem Heimatland bleiben, werden Sie sich weniger als Neuling fühlen, als wenn Sie nach Farawavia ziehen, wo alles anders funktioniert. Und dennoch werden Sie mehr wissen, wenn Sie umziehen. Das Gefühl, ein Neuling zu sein, steht also in umgekehrter Beziehung zur tatsächlichen Unwissenheit.
+</p>
+<p>Aber wenn das Gefühl, ein Neuling zu sein, gut für uns ist, warum mögen wir es dann nicht? Welchem evolutionären Zweck könnte eine solche Abneigung dienen?
+</p>
+<p>Ich denke, die Antwort ist, dass es zwei Quellen gibt, sich wie ein Neuling zu fühlen: dumm zu sein und etwas Neues zu tun. Unsere Abneigung, uns wie ein Neuling zu fühlen, ist unser Gehirn, das uns sagt: "Komm schon, komm schon, finde es heraus." Was für den größten Teil der Menschheitsgeschichte richtig war, zu denken. Das Leben der Jäger und Sammler war komplex, aber es hat sich nicht so sehr verändert wie das Leben jetzt. Sie mussten nicht plötzlich herausfinden, was sie mit der Kryptowährung tun sollten. Es war also sinnvoll, bei bestehenden Problemen gegenüber der Entdeckung neuer Probleme voreingenommen zu sein. Es machte Sinn, dass Menschen das Gefühl, ein Neuling zu sein, nicht mögen, genauso wie es in einer Welt, in der Nahrung knapp ist, Sinn machte, dass sie das Gefühl, hungrig zu sein, nicht mögen.
+</p>
+<p>Jetzt, da zu viel Nahrung eher ein Problem als zu wenig ist, führt uns unsere Abneigung gegen das Hungergefühl in die Irre. Und ich denke, unsere Abneigung gegen das Gefühl, sich wie ein Neuling zu fühlen, tut das auch.
+</p>
+<p>Obwohl es sich unangenehm anfühlt und die Leute Sie manchmal dafür verspotten werden, ist es umso besser, je mehr Sie sich wie ein Neuling fühlen.</p>
+            </div>
+          </body>
+        </div>
+    );
+      }
+    }
+
+export default Email;
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import Blog from "../containers/Blog";
 import Email from "../containers/Email";
 import Quotes from "../containers/Quotes";
 import App from "./../App";
+import Noob from "../containers/Noob";
 
 const publicRoutes = [
     {
@@ -43,6 +44,11 @@ const publicRoutes = [
         component: Quotes, // <-- this is the component that'll be rendered
         exact: true, // <-- this says to ONLY match when the path is exactly '/'
         path: "/Quotes" // <-- ... and this is the actual path to match on
+    },
+    {
+        component: Noob, // <-- this is the component that'll be rendered
+        exact: true, // <-- this says to ONLY match when the path is exactly '/'
+        path: "/Noob" // <-- ... and this is the actual path to match on
     },
 ];
 
