af976bf8cd76dc75256b3b580ba92190325b7f35
karan dehghani <karan@karans-mbp-2.speedport_w_921v_1_46_000>
2020-03-25 17:33:24 +0100

    new text

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -15,6 +15,11 @@ import Nav from '../../components/Nav/nav.js';
             <div id="content">
             <ul>
             <li><b>Blog - Each post has less than 100 chars.</b></li>
+            <li><h3>For the first time in history everybody on earth has the same wish.</h3>
+            <p>We will get an international institution with executive power.</p>
+            <p>March 25, 2020</p>
+            </li>
+
             <li><h3>Read the autobiography of Benjamin Franklin.</h3><p>It will help you to organize your life.</p>  <p>January 3, 2020</p></li>
           <li>
             <h3>Learn only what truly interests you.</h3> <p>It will help you avoid lying to yourself while learning.</p>
