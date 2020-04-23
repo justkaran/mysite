37740baf9b60042ac2f17715e41d630ab8d0b778
karan dehghani <karan@karans-mbp-2.speedport_w_921v_1_46_000>
2020-04-23 23:36:44 +0200

    new image

--- src/containers/Drawings/index.js
+++ src/containers/Drawings/index.js
@@ -16,6 +16,7 @@ import Nav from '../../components/Nav/nav.js';
             <img src="https://i.imgur.com/uoHSF1t.jpg" height="auto" width="400em" />
             <img src="https://i.imgur.com/L0cGTcL.jpg" height="auto" width="400em" />
             <img src="https://i.imgur.com/I8Wk4y3.png" height="auto" width="400em" />
+            <img src="https://i.imgur.com/4wXozmD.jpg" height="auto" width="400em" />
             <p>Drawing and painting helps me think and relax. Please don't take the images too seriously. </p>
             </div>
 
